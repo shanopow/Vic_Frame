0e47f19371e46e2294f39717352331b1d92f7f4b
shanopow <115043900+shanopow@users.noreply.github.com>
2023-09-22 13:35:00 +0100

    Added some dummy data

--- txt_reader.cpp
+++ txt_reader.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
+// a file when read should be represented as an array of lines, we have no need for multi-line concatting
+// first line is used differently as reference 
+class DataReader{
+    public:
+    std::vector<std::string> to_read;
+    
+
+    DataReader(std::vector<std::string> to_read){
+        this->to_read = to_read;
+    };
+
+    void File_Reader(){
+        for (auto i : this->to_read){
+            
+        }
+    };
+};
