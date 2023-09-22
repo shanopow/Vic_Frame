668ee100554cb3f9e13862825321fa7c5be71027
shanopow <115043900+shanopow@users.noreply.github.com>
2023-09-22 15:36:09 +0100

    Added Entry Class

--- txt_reader.cpp
+++ txt_reader.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 // a file when read should be represented as an array of lines, we have no need for multi-line concatting
 // first line is used differently as reference 
 class DataReader{
     public:
     std::vector<std::string> to_read;
-    
+    std::unordered_map<std::string, Entry> made_of;
 
     DataReader(std::vector<std::string> to_read){
         this->to_read = to_read;
@@ -16,7 +17,18 @@ class DataReader{
 
     void File_Reader(){
         for (auto i : this->to_read){
-            
+                    
         }
+    };
+};
+
+class Entry{
+    public:
+
+
+    std::string text;
+    
+    Entry(std::string text_taken){
+        this->text = text_taken;          
     };
 };
