9cd65be03b8b32b671bb37208cfbe00cbc6d836b
shanopow <115043900+shanopow@users.noreply.github.com>
2023-10-03 13:35:40 +0100

    txt_reader now reads files

--- txt_reader.cpp
+++ txt_reader.cpp
@@ -5,19 +5,26 @@
 #include <unordered_map>
 
 // a file when read should be represented as an array of lines, we have no need for multi-line concatting
-// first line is used differently as reference 
 class DataReader{
     public:
     std::vector<std::string> to_read;
-    std::unordered_map<std::string, Entry> made_of;
+    //std::unordered_map<std::string, Entry> made_of;
 
     DataReader(std::vector<std::string> to_read){
         this->to_read = to_read;
     };
 
     void File_Reader(){
+        std::string tmp_holder;
         for (auto i : this->to_read){
-                    
+            std::ifstream myfile (i);
+            
+            if ( myfile.is_open() ) {
+                while (myfile){
+                    myfile >> tmp_holder;
+                    std::cout << tmp_holder;   
+                }
+            }        
         }
     };
 };
@@ -25,10 +32,16 @@ class DataReader{
 class Entry{
     public:
 
-
     std::string text;
     
     Entry(std::string text_taken){
         this->text = text_taken;          
     };
 };
+
+int main(){
+    std::vector<std::string> holder;
+    holder.push_back("data/mapdata/provinces.txt");
+    DataReader *rah = new DataReader(holder);
+    rah->File_Reader();
+}
